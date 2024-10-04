1074c8f414892c0432c2ed42f42f9634c6deba03
saikiranyad <saisaikiran7890@gmail.com>
2024-10-04 07:15:22 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -76,7 +76,7 @@ const Footer = () => {
             <a href="" className="">
                 <img src="/images/logo.svg" width={40} height={40} alt="Logo" />
             </a>
-            <p className="text-zinc-500 text-sm">&copy;2024 <span className='text-zinc-200'>Saikiran "please give me job please"</span></p>
+            <p className="text-zinc-500 text-sm">&copy;2024 <span className='text-zinc-200'>Saikiran</span></p>
         </div>
     </div>
    </footer>
