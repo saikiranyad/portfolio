6f76914781bed260a6a80e092d5ab4802de36c57
saikiranyad <saisaikiran7890@gmail.com>
2024-10-04 07:13:48 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,24 +27,21 @@ const sitemap = [
   const socials = [
     {
       label: 'GitHub',
-      href: 'https://www.github.com/codewithsadee-org'
+     
     },
     {
       label: 'LinkedIn',
-      href: 'https://www.linkedin.com/in/codewithsadee'
+      
     },
     {
       label: 'Twitter X',
-      href: 'https://x.com/codewithsadee_'
+     
     },
     {
       label: 'Instagram',
-      href: 'https://www.instagram.com/codewithsadee'
+      
     },
-    {
-      label: 'CodePen',
-      href: 'https://codepen.io/codewithsadee'
-    }
+ 
   ];
 const Footer = () => {
   return (
